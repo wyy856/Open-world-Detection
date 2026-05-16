4f897adcc89464e6679048db356193d4191d92a0
Yue Wen <150776320+wyy856@users.noreply.github.com>
2026-05-16 17:15:44 +0800

    Register CH-HNN incremental module

--- mmdet_owod/owod/__init__.py
+++ mmdet_owod/owod/__init__.py
@@ -1,6 +1,7 @@
 import importlib.util
 
 from .wood_loss import WOODProposalLoss
+from .chhnn_incremental import CHHNNIncrementalModule
 
 if importlib.util.find_spec("mmdet") is not None and importlib.util.find_spec("mmengine") is not None:
     from .owod_bbox_head import OWODShared2FCBBoxHead
@@ -9,4 +10,4 @@ else:
     OWODShared2FCBBoxHead = None
     OWODMetric = None
 
-__all__ = ["WOODProposalLoss", "OWODShared2FCBBoxHead", "OWODMetric"]
+__all__ = ["WOODProposalLoss", "CHHNNIncrementalModule", "OWODShared2FCBBoxHead", "OWODMetric"]
